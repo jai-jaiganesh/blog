41dcb6b6a6d483701504fedd98c150e5361fea8c
jai <jg.jaiganesh3006@gmail.com>
2022-03-03 23:29:14 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID:process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-   callbackURL: "https://localhost:3000/auth/google/home",
+   callbackURL: "https://peaceful-chamber-62380.herokuapp.com/auth/google/home",
   userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
 },
 function(accessToken, refreshToken, profile, cb) {
